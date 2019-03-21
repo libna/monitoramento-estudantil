4f4a889f11cffb25e71ec4ae10f374b74ecd6c72
geovanejose <geovanejose240899@gmail.com>
2019-03-21 14:30:50 -0300

    #8 criação do cadastro e login dos usuários

---
+++ student-monitoring/resources/views/cadastro.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	
+	<form ac>
+		<input type="text" name="nome">
+		<br>
+		<input type="password" name="password">
+		<br>
+		<input type="password" name="confirmacaoDeSenha">
+		<input type="submit">
+	</form>
+
+</body>
+</html>
--- student-monitoring/routes/web.php
+++ student-monitoring/routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cadastro', function (){
+	return view('cadastro');
+});	
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
