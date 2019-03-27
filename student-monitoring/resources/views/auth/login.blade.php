3ff4a1ffb990c2e2c82db49005946aeaf2a2a43e
geovanejose <geovanejose240899@gmail.com>
2019-03-27 15:09:25 -0300

    #13 Alterando campo de matrícula no login do usuário

--- student-monitoring/resources/views/auth/login.blade.php
+++ student-monitoring/resources/views/auth/login.blade.php
@@ -12,14 +12,14 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="matrícula" class="col-md-4 col-form-label text-md-right">{{ __('Matrícula') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="matrícula" type="matrícula" class="form-control{{ $errors->has('matrícula') ? ' is-invalid' : '' }}" name="matrícula" value="{{ old('matrícula') }}" required autofocus>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('matrícula'))
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('matrícula') }}</strong>
                                     </span>
                                 @endif
                             </div>
