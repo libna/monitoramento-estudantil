1c5d5453124a83f65bceff92aad3c1550fd5e002
joselimabar <jrlima150305@gmail.com>
2019-03-28 14:04:39 -0300

    #16 modificando nome dos buttons do login

--- student-monitoring/resources/views/auth/login.blade.php
+++ student-monitoring/resources/views/auth/login.blade.php
@@ -45,7 +45,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Lembrar senha') }}
                                     </label>
                                 </div>
                             </div>
@@ -59,7 +59,7 @@
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Esqueceu sua senha?') }}
                                     </a>
                                 @endif
                             </div>
