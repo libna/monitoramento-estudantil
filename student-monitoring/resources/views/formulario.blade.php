cf05401e81b4a9f6a549a69617b5c156c5bd8028
Raul Barros <raulbarrosmr@gmail.com>
2019-03-28 14:16:43 -0300

    #14 Protótipo de Formulário feito pelo Bootstrap via CDN

--- student-monitoring/resources/views/formulario.blade.php
+++ student-monitoring/resources/views/formulario.blade.php
@@ -1,16 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<form>
-	<input type="text" name="pergunta">
-	<input type="submit">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div class="container">
+	<form>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Nome Completo:</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Exemplo: José da Silva">
+  </div>
+    <div class="form-group">
+    <label for="exampleFormControlSelect2">O que você cursou no Campus Igarassu:</label>
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+      <option>Informática para Internet</option>
+      <option>Logística</option>
+      <option>Gestão</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect2">Quantidade de Períodos do Curso:</label>
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect2">Quantidade de matérias do Período:</label>
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+      <option>6</option>
+      <option>7</option>
+      <option>8</option>
+
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Conte um pouco sobre sua experiência no Campus Igarassu:</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
 </form>
-
-
-</body>
+<button type="submit" class="btn btn-primary">Enviar Formulário</button>
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
