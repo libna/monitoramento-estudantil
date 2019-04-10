78f8620537c20b960771c442b873d72aa9585275
Wolaci <wolacijose8@gmail.com>
2019-04-10 13:05:59 -0300

    #27 criação da view do formulário de saída

---
+++ student-monitoring/resources/views/formularioexit.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+		<style>
+			body{
+				background-color: #fff;
+			}
+			.container{
+				margin-top: 20px;
+			}
+		</style>	
+    <title>Questionário #1</title>
+  </head>
+<body>
+  
+</body>
+</html>
