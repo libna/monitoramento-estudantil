67f4a7aefea1d58010cba4b80fa6e1eea0f6d78c
Raul Barros <raulbarrosmr@gmail.com>
2019-04-12 13:53:44 -0300

    #25 Incremento no welcome.blade.php

--- student-monitoring/resources/views/welcome.blade.php
+++ student-monitoring/resources/views/welcome.blade.php
@@ -8,6 +8,10 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
         <!-- Styles -->
         <style>
@@ -80,20 +84,49 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                 <div class="title m-b-md"> 
+                    <img src="{{asset('img/logos.png')}}" alt="Logo da Página" style="width: 100%;"> 
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentos</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">Notícias</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+<!--                     <a href="https://laravel.com/docs">Home</a>
+                    <a href="https://laracasts.com">Formulários</a>
+                    <a href="https://laravel-news.com">Sobre</a>
+                    <a href="https://blog.laravel.com">Ajuda</a>
+                    <a href="https://nova.laravel.com">Fórum</a>
+                    <a href="https://forge.laravel.com">Notícias</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a> -->
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand btn btn-link" href="#">Menu Principal</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link btn btn-danger mr-2" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link btn btn-warning mr-2" href="#">Formulário <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle btn btn-success mr-2" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
                 </div>
             </div>
         </div>
+        <script src="{{asset('js/app.js')}}" type="text/JavaScript"></script>
     </body>
-</html>
+    </html>
