25a1e81036bae2b7deed30438116b36e81f2d2cc
Raul Barros <raulbarrosmr@gmail.com>
2019-04-29 16:51:47 -0300

    #37 Passando o formulario.blade para a section 'content'

--- student-monitoring/resources/views/formulario.blade.php
+++ student-monitoring/resources/views/formulario.blade.php
@@ -1,36 +1,29 @@
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-		<style>
-			body{
-				background-color: #fff;
-			}
-			.container{
-				margin-top: 20px;
-			}
-		</style>	
-    <title>Questionário #1/1</title>
-  </head>
-  <body>
+@extends('layouts.app')
+
+@section('content')
     <div class="container">
-        <form action="{{ route('form') }}" method="POST">
-          @csrf
-            @foreach($perguntas as $pergunta)
-              <div class="form-group">
-                <label for="exampleFormControlInput1">
-                  {{$pergunta->titulo}}
-                </label>
-                  <input type="text" name="pergunta_{{ $pergunta->id }}" class="form-control" id="exampleFormControlInput1" placeholder=""> 
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header">Teste</div>
+              <div class="card-body">
+                <div class="container">
+                  <form action="{{ route('form') }}" method="POST">
+                    @csrf
+                      @foreach($perguntas as $pergunta)
+                        <div class="form-group">
+                          <label for="exampleFormControlInput1">
+                            {{$pergunta->titulo}}
+                          </label>
+                            <input type="text" name="pergunta_{{ $pergunta->id }}" class="form-control" id="exampleFormControlInput1" placeholder=""> 
+                        </div>
+                      @endforeach
+                    <button type="submit" class="btn btn-primary">Enviar Formulário</button>
+                  </form>
+                </div>
               </div>
-            @endforeach
-        <button type="submit" class="btn btn-primary">Enviar Formulário</button>
-        </form>
-    </div>
-  </body>
-</html>
+          </div>
+        </div>
+      </div>
+    </div>  
+@endsection
