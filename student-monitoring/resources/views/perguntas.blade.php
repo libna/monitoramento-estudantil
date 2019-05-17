5e863b49b5428e06ffc3730defeb3fc7f2ef2fcc
Wolaci <wolacijose8@gmail.com>
2019-05-17 15:59:26 -0300

    #54 Adicionando estilo no campo select

--- student-monitoring/resources/views/perguntas.blade.php
+++ student-monitoring/resources/views/perguntas.blade.php
@@ -6,7 +6,8 @@
 		<form>
 			<div class="form-group">
 				<input class="form-control" type="text" name="pergunta">
-				<select>
+				<select class="form-control" id="exampleFormControlSelect1" >
+						<option>Selecione o tipo da sua pergunta</option>
 					@foreach($tipoDePergunta as $tipo)
 						<option value="{{ $tipo->id}}">{{$tipo->tipo}}</option>
 					@endforeach
