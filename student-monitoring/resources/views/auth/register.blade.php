0b9c9b6bd15a53eb9f2b3c8b293c637fd40968ba
libna <libnarodrigues723@gmail.com>
2019-05-27 16:23:44 -0300

    #46 adicionado opções de cadastro

--- student-monitoring/resources/views/auth/register.blade.php
+++ student-monitoring/resources/views/auth/register.blade.php
@@ -9,19 +9,122 @@
 
                 <div class="card-body">
                     <div class="container">
-                        <!-- <div class="row"> -->
+                        
 
                             <div class=" btn-group-lg nav justify-content-center" role="group" aria-label="Basic example">
-                                <a href="" class="btn btn-dark m-2" role="button">Aluno</a>
-                                <a href="" class="btn btn-dark m-2" role="button">Egresso</a> 
-                                <a href="" class="btn btn-dark m-2" role="button">Professor</a>
+                                <a href="#" onclick="aluno()" id="alunos" class="btn btn-dark m-2" role="button">Aluno</a>
+                                <a href="#" onclick="egresso()" class="btn btn-dark m-2" role="button">Egresso</a> 
+                                <a href="#" onclick="prof()" class="btn btn-dark m-2" role="button">Professor</a>
+                            </div>
+                            
+                            <div class="d-none" id="aluno" >
+
+                                                <div class="card-body">
+                                                    <form method="POST" action="{{ route('register') }}">
+                                                        @csrf
+                                                        <div class="form-group row">
+                                                            <label for="matricula" class="col-md-4 col-form-label text-md-right">{{ __('Matrícula') }}</label>
+
+                                                            <div class="col-md-6">
+                                                                <input id="matricula" type="text" class="form-control{{ $errors->has('matricula') ? ' is-invalid' : '' }}" name="matricula" value="{{ old('matricula') }}" required autofocus>
+
+                                                                @if ($errors->has('matricula'))
+                                                                    <span class="invalid-feedback" role="alert">
+                                                                        <strong>{{ $errors->first('matricula') }}</strong>
+                                                                    </span>
+                                                                @endif
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group row">
+                                                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+
+                                                            <div class="col-md-6">
+                                                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+
+                                                                @if ($errors->has('name'))
+                                                                    <span class="invalid-feedback" role="alert">
+                                                                        <strong>{{ $errors->first('name') }}</strong>
+                                                                    </span>
+                                                                @endif
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group row">
+                                                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Endereço de E-mail') }}</label>
+
+                                                            <div class="col-md-6">
+                                                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+
+                                                                @if ($errors->has('email'))
+                                                                    <span class="invalid-feedback" role="alert">
+                                                                        <strong>{{ $errors->first('email') }}</strong>
+                                                                    </span>
+                                                                @endif
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group row">
+                                                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
+
+                                                            <div class="col-md-6">
+                                                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                                                @if ($errors->has('password'))
+                                                                    <span class="invalid-feedback" role="alert">
+                                                                        <strong>{{ $errors->first('password') }}</strong>
+                                                                    </span>
+                                                                @endif
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group row">
+                                                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmação de senha') }}</label>
+
+                                                            <div class="col-md-6">
+                                                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group row mb-0">
+                                                            <div class="col-md-6 offset-md-4">
+                                                                <button type="submit" class="btn btn-success">
+                                                                    {{ __('Fazer registro') }}
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                           
+                             <div class="d-none" id="egresso" >
+                                Egresso
                             </div>
 
-                        <!-- </div> -->
+                             <div class="d-none" id="professor">
+                                Professor
+                            </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script type="text/javascript">
+
+    function aluno(){
+        document.getElementById("aluno").className     = "";
+        document.getElementById("egresso").className   = "d-none";
+        document.getElementById("professor").className = "d-none";
+    }
+    function egresso(){
+        document.getElementById("egresso").className   = "";
+        document.getElementById("professor").className = "d-none";
+        document.getElementById("aluno").className     = "d-none";
+    }
+    function prof(){
+        document.getElementById("professor").className = "";
+        document.getElementById("aluno").className     = "d-none";
+        document.getElementById("egresso").className   = "d-none";
+    }
+</script>
 @endsection
