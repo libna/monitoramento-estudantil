446f69e74cc72581fb6bc17533b7b53ea6647ede
Raul Barros <raulbarrosmr@gmail.com>
2019-06-01 11:20:22 -0300

    #46 Adição de elementos no front-end do formulário

--- student-monitoring/resources/views/formulario.blade.php
+++ student-monitoring/resources/views/formulario.blade.php
@@ -8,7 +8,7 @@
             <div class="card-header">{{$perguntas[0]->titulo}}</div>
               <div class="card-body">
                 <div class="container">
-                  <form action="{{ route('formsave') }}" method="POST">
+                  <form action="{{ route('formsave') }}" method="POST" class="mb-4">
                     @csrf
                       <input type="hidden" name="form_id" value="{{ $id }}">
                       @foreach($perguntas[0]->perguntas as $pergunta)
@@ -20,6 +20,7 @@
                         </div>
                       @endforeach
                     <button type="submit" class="btn btn-primary">Enviar Formulário</button>
+                    <a href="{{asset('home')}}" class="ml-3" role="button">Voltar</a>
                   </form>
                 </div>
               </div>
--- student-monitoring/resources/views/home.blade.php
+++ student-monitoring/resources/views/home.blade.php
@@ -6,12 +6,12 @@
       <div class="row justify-content-center">
   @foreach($formulario as $form)
    
-    <div class="card" style="width: 18rem;">
+    <div class="card mr-2 mb-2" style="width: 18rem;">
        <div class="card-header">{{$form->titulo}}</div>
         <img src="{{asset('img/form-icon-2.png')}}" alt="Ícone de Formulário" class="card-img-top" style="padding: 10px;">
         <div class="card-body">
            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="{{route('form', $form->id)}}" class="btn btn-primary"> Go somewhere</a>
+            <a href="{{route('form', $form->id)}}" class="btn btn-primary">Entrar</a>
         </div>
 
     </div>
