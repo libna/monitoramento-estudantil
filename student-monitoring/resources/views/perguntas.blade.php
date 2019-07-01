b9b8bb2e8b5a98ceb8b2333529a590ea9c98eec7
Raul Barros <raulbarrosmr@gmail.com>
2019-07-01 15:26:48 -0300

    #46 Correção de textos e nas margens dos inputs e select

--- student-monitoring/resources/views/perguntas.blade.php
+++ student-monitoring/resources/views/perguntas.blade.php
@@ -2,23 +2,23 @@
 
 @section('content')
 	<div class="container" >
-		<label>cadastre a pergunta no formulário criado</label>
+		<label>Cadastre a pergunta no formulário criado</label>
 		<form action="{{ route('pergSave') }}" method="POST" >
 		@csrf
 			<div class="form-group" >
 				<input type="hidden" name="id" value="{{ $id }}">
 
 				<input class="form-control" type="text" name="pergunta">
-				<select class="form-control" id="exampleFormControlSelect1" required name="tipoPergunta">
+				<select class="form-control mt-2" id="exampleFormControlSelect1" required name="tipoPergunta">
 						<option value="1" >Selecione o tipo da sua pergunta</option>
 					@foreach($tipoDePergunta as $tipo)
 						<option value="{{$tipo->id}}">{{$tipo->tipo}}</option>
 					@endforeach
 				</select>
-				<input class="btn btn-primary" type="submit" value="cadastre sua pergunta">
-			</div>	
+				<input class="btn btn-primary mt-2" type="submit" value="Cadastar Pergunta">
+			</div>
 		</form>
-		
-		
-	</div>	
+
+
+	</div>
 @endsection
