fad58cfd5a5963e0e6161de649fed20511d12c5b
Raul Barros <raulbarrosmr@gmail.com>
2019-07-01 15:15:08 -0300

    #46 Estilizando página de confirmação de registro de respostas

--- student-monitoring/resources/views/register.blade.php
+++ student-monitoring/resources/views/register.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-	<div>
-		Respostas cadastradas com sucesso!!!!!!!
-		<br>
-		<a href="{{ route('home') }}" class="btn btn-primary"> Voltar para o Home </a>
+<div class="container">
+	<div class="alert alert-success" role="alert">
+		Respostas cadastradas com sucesso!
 	</div>
-@endsection	
+		<a href="{{ route('home') }}" class="btn btn-primary"> Voltar para o Home</a>
+</div>
+
+@endsection
